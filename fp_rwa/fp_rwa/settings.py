ee840a468c52cde655bc5282588eaa3c92b1b566
sambhuvaneswari <88283183+sambhuvaneswari@users.noreply.github.com>
2021-08-01 16:38:21 -0400

    Update settings.py

--- fp_rwa/fp_rwa/settings.py
+++ fp_rwa/fp_rwa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'h223p9rw*i4a)j^@ln6s7-m8fn#vm33uv)=wddqdqt8t6+e2g1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'fp-sam-rwa-08012021.azurewebsites.net']
 
 
 # Application definition
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'fp_rwa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '{MYNAME}',
+        'USER': '{MYUSER}',
+        'PASSWORD': '{MYPASSWORD}',
+        'HOST': '{MYDB}.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -115,7 +124,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
